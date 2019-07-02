b83c1899b1a47d58f63fbe854379a8293dc93bbc
Karl Söderberg <karl.soderberg@gmail.com>
2019-07-03 00:04:58 +0200

    Initial Commit

---
+++ BetalaP2/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  BetalaP2
+//
+//  Created by Karl Söderberg on 2019-07-03.
+//  Copyright © 2019 Karl Söderberg. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
